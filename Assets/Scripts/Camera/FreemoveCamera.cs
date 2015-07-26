119e098a4cd9be7e5b9026bb3d5b44b2c84fc6b7
Brian Erikson <briankerikson@gmail.com>
2015-07-26 14:00:03 +0200

    adjusted freemove camera look and is now using action component to remove blocks.

--- Assets/Scripts/Camera/FreemoveCamera.cs
+++ Assets/Scripts/Camera/FreemoveCamera.cs
@@ -1,52 +1,29 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.CrossPlatformInput;
 using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
 
-public class FreemoveCamera : MonoBehaviour {
+public class FreemoveCamera : NetworkBehaviour {
+	public float XSensitivity = 2f;
+	public float YSensitivity = 2f;
 	public static int MOVE_SPEED = 10;
 
-	private GameObject world;
-
-	Vector3 lastMousePos = new Vector3(0,0);
-	bool mouseDown = false;
 
 	// Use this for initialization
 	void Start () {
-		transform.Translate(20f, 25f, 0f);
-		transform.Rotate(new Vector3(45, -45));
 
-		world = GameObject.FindGameObjectWithTag("World");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		UpdateInput();
-
 	}
 
+	[Client]
 	void UpdateInput() {
 		UpdateMovement();
 		UpdateMouselook();
-		UpdateActions();
-	}
-
-	void UpdateActions() {
-		if (Input.GetKey(KeyCode.Space)) {
-			Vector3 origin = transform.position;
-			Vector3 dir = transform.forward;
-			Ray ray = new Ray(origin, dir);
-			RaycastHit hit;
-			if (Physics.Raycast(ray, out hit)) {
-				GameObject obj = hit.transform.gameObject;
-				if (obj.CompareTag("Chunk")) {
-					int x = Mathf.FloorToInt(hit.point.x);
-					int y = Mathf.FloorToInt(hit.point.y);
-					int z = Mathf.FloorToInt(hit.point.z);
-
-					World script = world.GetComponent("World") as World;
-					script.CmdRemoveBlock(x, y, z);
-				}
-			}
-		}
 	}
 
 	void UpdateMovement() {
@@ -68,15 +45,11 @@ public class FreemoveCamera : MonoBehaviour {
 	}
 
 	void UpdateMouselook() {
-		if (Input.GetMouseButtonDown(0)) mouseDown = true;
-		else if (Input.GetMouseButtonUp(0)) mouseDown = false;
+		float yRot = CrossPlatformInputManager.GetAxis("Mouse X") * XSensitivity;
+		float xRot = CrossPlatformInputManager.GetAxis("Mouse Y") * YSensitivity;
 
-		if (mouseDown) {
-			Vector3 deltaPos = Input.mousePosition - lastMousePos;
-			lastMousePos = Input.mousePosition;
-			
-			transform.Rotate(new Vector3(-deltaPos.y, deltaPos.x));
-			transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0f);
-		}
+		transform.localRotation *= Quaternion.Euler (0f, yRot, 0f);
+		transform.localRotation *= Quaternion.Euler (-xRot, 0f, 0f);
+		transform.rotation = Quaternion.Euler(new Vector3(transform.localRotation.eulerAngles.x, transform.localRotation.eulerAngles.y, 0f));
 	}
 }
